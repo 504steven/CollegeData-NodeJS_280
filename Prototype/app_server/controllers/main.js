da6a5716eecf03a17eb8ac2a5dd1193f260c8e5a
Yimu Yang <yimuyang19911223@gmail.com>
2019-09-19 14:34:05 -0700

    update dynamic page

--- Prototype/app_server/controllers/main.js
+++ Prototype/app_server/controllers/main.js
@@ -1,4 +1,11 @@
 var lineReader = require('line-reader');
+var sjsu = {params: {Name: 'San Jose State University', Location: 'San Jose, CA', Acceptance_rate: '53.4%', Graduation_rate: '51.6%',
+        Average_annual_cost: '27,039 USD', Average_salary_after_attending_undergrad: '56,100 USD'}};
+var stanford = {params: {Name: 'Stanford University', Location: 'Stanford, CA', Acceptance_rate: '5%', Graduation_rate: '95%',
+        Average_annual_cost: '14,000 USD'}};
+var berkeley = {params: {Name: 'University of California, Berkeley', Location: 'Berkeley, CA', Acceptance_rate: '17%', Graduation_rate: '91%',
+        Average_annual_cost: '14,000 USD'}};
+
 
 module.exports.home = function(req, res) {
     sendPage('public/html/index.html', res);
@@ -7,7 +14,11 @@ module.exports.home = function(req, res) {
 module.exports.getCollegeInfo = function(req, res) {
     var schoolName = getCollegeName(req);
     if (schoolName == "SJSU" || schoolName == "sjsu" || schoolName == "San Jose State University") {
-        res.render('universityInfo');
+        res.render('universityInfo', sjsu);
+    } else if (schoolName == "Stanford" || schoolName == "stanford" || schoolName == "Stanford University") {
+        res.render('universityInfo', stanford);
+    } else if (schoolName == "UCB" || schoolName == "ucb" || schoolName == "University of California, Berkeley") {
+        res.render('universityInfo', berkeley);
     } else {
         sendPage('public/html/error.html', res)
     }

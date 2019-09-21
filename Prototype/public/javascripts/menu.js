fe763ad754307a2cb673a60d56d24e49bf5c751a
yijun zhang <zhangyijun0518@gmail.com>
2019-09-21 00:13:01 -0700

    add animation menu

---
+++ Prototype/public/javascripts/menu.js
@@ -0,0 +1,40 @@
+window.onload = init;
+
+function init()
+{
+    var allLinks = document.getElementsByTagName("a");
+
+    for (var i = 0; i < allLinks.length; i++) {
+        if (allLinks[i].className == "menuLink"){
+            allLinks[i].onmouseover = toggleMenu;
+            allLinks[i].onclick = clickHandler;
+        }
+    }
+}
+
+function clickHandler (event)
+{
+    event.preventDefault();
+}
+
+function toggleMenu()
+{
+    var startMenu   = this.href.lastIndexOf("/") + 1;
+    var stopMenu   = this.href.lastIndexOf(".");
+    var thisMenuName = this.href.substring(startMenu,stopMenu);
+
+    var menuParent = document.getElementById(thisMenuName).parentNode;
+    var thisMenuStyle = document.getElementById(thisMenuName).style;
+
+    thisMenuStyle.display = "block";
+
+    menuParent.onmouseout = function()
+    {
+        thisMenuStyle.display = "none";
+    };
+
+    menuParent.onmouseover = function()
+    {
+        thisMenuStyle.display = "block";
+    };
+}

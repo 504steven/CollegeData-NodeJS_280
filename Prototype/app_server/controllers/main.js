4e4bfaa34da4b33b2b2057cbcdebd67bbb838e32
Yimu Yang <yimuyang19911223@gmail.com>
2019-09-26 11:18:03 -0700

    modify format

--- Prototype/app_server/controllers/main.js
+++ Prototype/app_server/controllers/main.js
@@ -2,34 +2,60 @@
 var readline = require('readline');
 var fs = require('fs');
 var isWin = process.platform === "win32";
-var projectfolername = isWin? 'Prototype/' : '';
+var projectfolername = isWin ? 'Prototype/' : '';
 
-var sjsu = {name: 'San Jose State University', params: {Name: 'San Jose State University', Location: 'San Jose, CA', Acceptance_rate: '53.4%', Graduation_rate: '51.6%', Average_annual_cost: '27,039 USD', Average_salary_after_attending_undergrad: '56,100 USD'}};
-var stanford = {name: 'Stanford University', params: {Name: 'Stanford University', Location: 'Stanford, CA', Acceptance_rate: '5%', Graduation_rate: '95%', Average_annual_cost: '14,000 USD'}};
-var berkeley = {name: 'University of California, Berkeley', params: {Name: 'University of California, Berkeley', Location: 'Berkeley, CA', Acceptance_rate: '17%', Graduation_rate: '91%', Average_annual_cost: '14,000 USD'}};
+var sjsu = {name: 'San Jose State University',
+    params: {
+        Name: 'San Jose State University',
+        Location: 'San Jose, CA',
+        Acceptance_rate: '53.4%',
+        Graduation_rate: '51.6%',
+        Average_annual_cost: '27,039 USD',
+        Average_salary_after_attending_undergrad: '56,100 USD'
+    }
+};
+
+var stanford = {name: 'Stanford University',
+    params: {
+        Name: 'Stanford University',
+        Location: 'Stanford, CA',
+        Acceptance_rate: '5%',
+        Graduation_rate: '95%',
+        Average_annual_cost: '14,000 USD'
+    }
+};
+
+var berkeley = {name: 'University of California, Berkeley',
+    params: {
+        Name: 'University of California, Berkeley',
+        Location: 'Berkeley, CA',
+        Acceptance_rate: '17%',
+        Graduation_rate: '91%',
+        Average_annual_cost: '14,000 USD'
+    }
+};
 
 var registeredUsers = [];
 
 module.exports.home = function(req, res) {
-    sendPage(  projectfolername + 'public/html/loginAndRegister.html', res);
-}
-
-// module.exports.get_register = function(req, res) { }
+    sendPage(projectfolername + 'public/html/loginAndRegister.html', res);
+};
 
 module.exports.post_register = function(req, res) {
     console.log('register called');
-    // console.log(req.body.username);
-    if(!req.body.username || !req.body.password) {
-        res.send("Missing Username or Password for Registeration !");
-    }else {
-        var matches = registeredUsers.filter(function(user) {
+    if (!req.body.username || !req.body.password) {
+        res.send("Missing Username or Password for Registeration!");
+    } else {
+        var matches = registeredUsers.filter(function (user) {
             return user.username == req.body.username;
         });
-        if(matches.length > 0) {
+        if (matches.length > 0) {
             res.send("username has been registered. use another username or try login");
-        }else {
-            var newUser = {username: req.body.username,
-                            password: req.body.password};
+        } else {
+            var newUser = {
+                username: req.body.username,
+                password: req.body.password
+            };
             registeredUsers.push(newUser);
             // console.log(newUser);
             req.session.user = newUser;
@@ -37,45 +63,43 @@ module.exports.post_register = function(req, res) {
             res.redirect('/protected');
         }
     }
-}
+};
 
 
 module.exports.post_login = function(req, res) {
-    console.log( 'login called');
-    var matches = registeredUsers.filter(function(user){
+    console.log('login called');
+    var matches = registeredUsers.filter(function(user) {
         return user.username == req.body.username && user.password == req.body.password;
     });
-    if(matches.length == 0) {
+    if (matches.length == 0) {
         res.send('wrong username or password');
-    }else {
+    } else {
         // console.log(matches[0]);
         req.session.user = matches[0];
         res.redirect('/protected');
     }
-}
-
-// module.exports.get_login = function(req, res) {  }
+};
 
 module.exports.get_logout = function(req, res) {
-    if(req.session.user) {
-        req.session.destroy( function() {
+    if (req.session.user) {
+        req.session.destroy(function() {
             console.log('session deleted');
         });
     }
     res.redirect('/');
-}
+};
 
 module.exports.loggedIn = function(req, res, next) {
-    if(req.session.user) {
+    if (req.session.user) {
         next();
-    }else {
-        res.send("must log in first！");
+    } else {
+        res.send("must log in first");
     }
-}
+};
 
 module.exports.get_protected = function(req, res) {
-    sendPage(  projectfolername + 'public/html/index.html', res);
-}
+    sendPage(projectfolername + 'public/html/index.html', res);
+};
 
 module.exports.getCollegeInfo = function(req, res) {
     console.log("finding college");
@@ -91,13 +115,11 @@ module.exports.getCollegeInfo = function(req, res) {
         //sendPage('public/html/error.html', res);
         console.log("send error page");
     }
-}
+};
 
 module.exports.get_overview = function(req, res) {
     sendPage(projectfolername + 'public/html/overview.html', res);
-}
-
-
+};
 
 function sendPage(filename, res) {
     var html = '';
@@ -110,24 +132,21 @@ function sendPage(filename, res) {
     //readInterface is async
     readInterface.on('line', function(line) {
         html += line + '\n';
-    }).on('close', function(){
+    }).on('close', function() {
         res.send(html);
     });
-    // console.log("sent html page");
-    // html = '';
 }
-
 
 function sendPageOld(fileName, result) {
     var html = '';
 
     lineReader.eachLine(fileName, function(line, last) {
-            html += line + '\n';
-            if (last) {
-                result.send(html);
-                return false;
-            } else {
-                return true;
-            }
-        });
+        html += line + '\n';
+        if (last) {
+            result.send(html);
+            return false;
+        } else {
+            return true;
+        }
+    });
 }
--- Prototype/public/javascripts/onload.js
+++ Prototype/public/javascripts/onload.js
@@ -1,30 +1,39 @@
 window.onload = init;
+var i = 1;
 
-function init()
-{
+function init() {
+    // dropdown menu init
     var allLinks = document.getElementsByTagName("a");
-
     for (var i = 0; i < allLinks.length; i++) {
-        if (allLinks[i].className == "menuLink"){
+        if (allLinks[i].className == "menuLink") {
             allLinks[i].onmouseover = toggleMenu;
             allLinks[i].onclick = clickHandler;
         }
     }
+    // image animation init
+    window.setInterval("changeImg()", 1200);
 }
 
-function clickHandler (event)
-{
+function changeImg() {
+    i++;
+    if (i > 3) {
+        i = 1;
+    }
+    var img = document.getElementById("changeImg");
+    img.src = "/img/" + i + ".jpg";
+}
+
+function clickHandler(event) {
     event.preventDefault();
 }
 
-function toggleMenu()
-{
-    var startMenu   = this.href.lastIndexOf("/") + 1;
-    var stopMenu   = this.href.lastIndexOf(".");
-    var thisMenuName = this.href.substring(startMenu,stopMenu);
-
+function toggleMenu() {
+    var startMenu = this.href.lastIndexOf("/") + 1;
+    var stopMenu = this.href.lastIndexOf(".");
+    var thisMenuName = this.href.substring(startMenu, stopMenu);
     var menuParent = document.getElementById(thisMenuName).parentNode;
     var thisMenuStyle = document.getElementById(thisMenuName).style;
+
     thisMenuStyle.display = "block";
 
     menuParent.onmouseout = function() {

0bb1fa028f2a7b65aa916c211340dc8fb7f3b151
504steven <504steven@gmail.com>
2019-10-20 22:03:18 -0700

    update package

--- Prototype-jQuery/public/javascripts/onload.js
+++ Prototype-jQuery/public/javascripts/onload.js
@@ -42,8 +42,6 @@ function initAll() {
     $("#dialog").dialog();
     $("#dialog").dialog("close");
 
-    // $("#selectable").selectable();
-
     // build sortable user fav college list
     $("#sortable").sortable();
 }
